7485da4250a218105246a4065b80651bca7eebc1
mjniday <michael.niday@gmail.com>
2016-08-12 01:16:55 -0400

    Updating mailgun in configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,20 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "fanfor.today",
+    :user_name => "postmaster@fanfor.today",
+    :password => "474b6996cc3d571d64b6959d31b1a2d7"
+  }
+
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
